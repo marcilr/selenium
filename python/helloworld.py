cdb18dbf5967f2cabf36bdc0dc28ebfe57396f8c
Raymond E. Marcil <remarcil@alaska.gov>
2019-06-18 11:07:13 -0800

    helloworld.py   This program prints Hello, world!

---
+++ python/helloworld.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# helloworld.py
+# Created Tue Jun 18 10:57:57 AKDT 2019
+# Copyright (C) 2019 by Raymond E. Marcil <marcilr@gmail.com>
+#
+# This program prints Hello, world!
+#
+print('Hello, world!')
