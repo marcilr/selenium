085f24cd152fad04e5e5685bfa6e2a83ac400290
Raymond E. Marcil <remarcil@alaska.gov>
2019-06-21 09:21:18 -0800

    Added links to header comment. Added initial test from:   ChromeDriver - WebDriver for Chrome   Getting started   https://sites.google.com/a/chromium.org/chromedriver/getting-started

--- webdriver/chromedriver/tests/test1.py
+++ webdriver/chromedriver/tests/test1.py
@@ -2,5 +2,24 @@
 # test1.py
 # Created Fri Jun 21 09:18:52 AKDT 2019
 # Copyright (C) 2019 by Raymond E. Marcil <marcilr@gmail.com>
-
+#
+# Links
+# =====
+# Getting started
+# ChromeDriver - WebDriver for Chrome
+# https://sites.google.com/a/chromium.org/chromedriver/getting-started
+#
 print("Test 1")
+
+import time
+
+from selenium import webdriver
+import selenium.webdriver.chrome.service as service
+
+service = service.Service('/path/to/chromedriver')
+service.start()
+capabilities = {'chrome.binary': '/path/to/custom/chrome'}
+driver = webdriver.Remote(service.service_url, capabilities)
+driver.get('http://www.google.com/xhtml');
+time.sleep(5) # Let the user actually see something!
+driver.quit()
